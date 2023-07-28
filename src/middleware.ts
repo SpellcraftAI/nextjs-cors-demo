fbb034c1ac9d0949ab3c766390032f7b8bb4eae9
Lewis <lewis@spellcraft.org>
2023-07-28 04:51:10 -0500

    chore: use `*` origin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,15 @@
 import { CorsMiddleware } from "nextjs-edge-cors";
 
 /**
- * Demo: Cross-origin will only be accessible from google.com. Set to "*" (or
- * remove `origin` override) to allow all origins.
+ * Allow requests from all origins.
  *
  * @demo
  * ```
- * await fetch("http://localhost:3000/api/cors")
+ * await fetch("https://nextjs-edge-cors.vercel.app/api/cors")
  *```
- *
- * *Access to fetch at 'http://localhost:3000/api/cors' from origin
- * 'https://www.godaddy.com' has been blocked by CORS policy: The
- * 'Access-Control-Allow-Origin' header has a value 'https://www.google.com'
- * that is not equal to the supplied origin.*
  */
 export const middleware = CorsMiddleware({
-  origin: "https://www.google.com"
+  origin: "*"
 })
 
 /**
